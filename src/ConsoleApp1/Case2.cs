bea4421e48188678b82ec08968fbc4b19e94c281
심승용 <seungyongshim@nexon.co.kr>
2023-06-21 09:07:06 +0900

    .

---
+++ src/ConsoleApp1/Case2.cs
@@ -0,0 +1,22 @@
+public class Case2
+{
+    public static async Task RunAsync()
+    {
+        var a = 0;
+
+        async Task<int> AddAsync()
+        {
+            await Task.Delay(0);
+            return a++;
+        }
+
+        var q = from _1 in unitAff
+                let f = Aff(AddAsync().ToValue)
+                from _2 in f.Repeat(Schedule.Once | Schedule.repeat(1))
+                select unit;
+
+        var r = await q.Run();
+
+        Console.WriteLine($"Case2: {a}");
+    }
+}
---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,2 @@
+await Case1.RunAsync();
+await Case2.RunAsync();
